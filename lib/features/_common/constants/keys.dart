8743626e17b7851f1a997fea2d6f6ccffcfbfe6b
Orji Emmanuel Kelechi <97282067+MrKhay@users.noreply.github.com>
2024-07-17 04:26:21 -0700

    Delete lib/features/_common/constants/keys.dart

--- lib/features/_common/constants/keys.dart
+++
@@ -1,6 +0,0 @@
-// ignore_for_file: public_member_api_docs
-
-const String kTimuEmail = 'orjikelechi25@gmail.com';
-const String kTimuPassword = '7r2QjaYRTihbZDd';
-
-const String kTimuOrganisationID = '0bc334ae65124c44b6dd5e76825b08bc';
